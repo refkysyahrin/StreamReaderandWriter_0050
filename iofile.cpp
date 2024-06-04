83cd697373e330acdb0d81d7c37ddbcd1484e325
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-06-04 17:27:42 +0700

    menulis dan memasukan nilai dari 'baris' ke dalam file

--- iofile.cpp
+++ iofile.cpp
@@ -20,7 +20,8 @@ int main() {
 		getline(cin, baris);
 		//loop akan berhenti jika anda memasukan karakter q
 		if (baris == "q") break;
-		
+		//menulis dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
 	}
 
 }
