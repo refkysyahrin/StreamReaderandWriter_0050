21afb04beddec8580514e88b9db854a04bad5135
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-06-04 17:23:11 +0700

    Membuka file dalam mode menulis.

--- iofile.cpp
+++ iofile.cpp
@@ -1,3 +1,11 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+using namespace std;
+
+int main() {
+    string baris;
+
+    // Membuka file dalam mode menulis.
+    offstream outfile;
+}
