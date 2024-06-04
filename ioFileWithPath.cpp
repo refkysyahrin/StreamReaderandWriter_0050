4cd9682a0ff5cd41ce71eb6dd8aa5f0be8ac5ed4
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-06-04 17:48:14 +0700

    jika tidak di temukan file maka akan menampilkan ini

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -14,4 +14,42 @@ int main() {
     ofstream outfile;
     //menunujuk ke sebuah nama file
     outfile.open(NamaFile + ".txt" , ios::out);
+
+    cout << ">= Menulis file, \'q\q untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q") break;
+		//menulis dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
+	//selesai dalam menulis sekarang tutup filenya
+	outfile.close();
+
+	//Membuka file dalam mode membaca
+	ifstream infile;
+	//menunjuk ke sebuah file
+	infile.open(NamaFile + ".txt", ios::in);
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika file ada maka
+	if (infile.is_open())
+	{
+		//melakukan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			//dan tampilkan di sini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
+    //jika tidak di temukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
+
 }
