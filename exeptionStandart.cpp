4f05043adb96470317ddbeb6ae458bbd299a60bf
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-06-04 18:34:32 +0700

    /*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/

--- exeptionStandart.cpp
+++ exeptionStandart.cpp
@@ -16,9 +16,9 @@ int main()
 	catch (exception& e) {
 		//penangkap menggunakan obyek exception
 		cout << e.what() << endl;
-		//akan dieksekusi karna array data hanya memiliki 3 elemen
+		/*akan dieksekusi karna array data hanya memiliki 3 elemen*/
 	}
 	cout << "Baris Program Yang terakhir" << endl;
-	/penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan/
+	/*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
 	return 0;
-} 
+}
